840263a9fc36c5259722834fe335d045fff108e5
AbhiKumar98 <abhishekkumar35962@gmail.com>
2020-12-19 00:59:19 +0530

    feat: added support files in db

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,117 +1,236 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dataCollectionRef, db } from "../utils/firebase";
 import firebase from "firebase";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+interface Document {
+	textContent?: string;
+	storageURL?: string;
+	deleteDate: Date;
+	id: string;
+}
+
 const Upload = () => {
-    const router = useRouter();
+	const router = useRouter();
 
-    const [isText, setIsText] = useState(false);
-    const [isFiles, setIsFiles] = useState(false);
+	const [isText, setIsText] = useState(false);
+	const [isFiles, setIsFiles] = useState(false);
 
-    const [textContent, setTextContent] = useState("");
+	const [textContent, setTextContent] = useState("");
 
-    const [isUploading, setIsUploading] = useState(false);
+	const [isUploading, setIsUploading] = useState(false);
+	const [files, setFiles] = useState<File[]>([]);
 
-    const signUpAnonymously = async () => {
-        await firebase.auth().signInAnonymously();
-    };
+	const signUpAnonymously = async () => {
+		await firebase.auth().signInAnonymously();
+	};
 
-    const uploadDate = async () => {
-        try {
-            const docRef = dataCollectionRef;
+	const uploadDate = async () => {
+		try {
+			const docRef = dataCollectionRef;
 
-            await signUpAnonymously();
+			await signUpAnonymously();
 
-            await docRef.set({
-                text: textContent,
-            });
+			const deleteOn = new Date();
+			deleteOn.setDate(deleteOn.getDate() + 1);
 
-            router.push(`/code?code=${docRef.id}`);
+			const savingDocument: Document = {
+				id: docRef.id,
+				deleteDate: deleteOn,
+			};
 
-            setIsUploading(true);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsText(false);
-            setTextContent("");
-            setIsFiles(false);
-            setIsUploading(false);
-        }
-    };
+			if (isText && textContent.length) {
+				savingDocument.textContent = textContent;
+			}
 
-    useEffect(() => {
-        return () => {
-            firebase.auth().signOut();
-        };
-    }, []);
+			if (isFiles && files.length) {
+				const firebaseStorageRef = firebase
+					.storage()
+					.ref(`${docRef.id}/${files[0].name}`);
+				await firebaseStorageRef.put(files[0]);
+				const downloadURL = await firebaseStorageRef.getDownloadURL();
 
-    return (
-        <div className="w-screen h-screen bg-blue-50 overflow-scroll">
-            <div className="text-4xl py-5 text-center font-bold font-sans">
-                Upload files
-            </div>
-            <div className="text-xl py-5 w-3/4 font-sans flex mx-auto justify-center mb-10 text-center">
-                Your data will be uploaded anonymously and will be deleted
-                automatically after 24 hours
-            </div>
-            <div className="flex mx-auto w-1/2 flex-col">
-                <div>
-                    <label className="inline-flex items-center">
-                        <input
-                            disabled={isUploading}
-                            type="checkbox"
-                            className="form-checkbox text-blue-600"
-                            checked={isText}
-                            onChange={(e) => {
-                                if (!e.target.checked) {
-                                    setTextContent("");
-                                }
-                                setIsText(e.target.checked);
-                            }}
-                        />
-                        <span className="ml-2">
-                            Do you have text content that you want to share?
-                        </span>
-                    </label>
-                </div>
-                {isText && (
-                    <div className="my-4">
-                        <textarea
-                            disabled={isUploading}
-                            value={textContent}
-                            onChange={(e) => setTextContent(e.target.value)}
-                            className="form-textarea resize-y border rounded-md w-full max-w-full"
-                        />
-                    </div>
-                )}
-                <div>
-                    <label className="inline-flex items-center">
-                        <input
-                            disabled={isUploading}
-                            type="checkbox"
-                            className="form-checkbox text-blue-600"
-                            checked={isFiles}
-                            onChange={(e) => setIsFiles(e.target.checked)}
-                        />
-                        <span className="ml-2">
-                            Do you have files that you want to share?
-                        </span>
-                    </label>
-                </div>
+				console.log(downloadURL);
 
-                <button
-                    onClick={uploadDate}
-                    className={`my-5 bg-blue-500 w-min mx-auto py-2 px-4 rounded-sm text-white focus:ring-1 flex ${
-                        isUploading ? "bg-gray-400" : ""
-                    }`}
-                >
-                    Upload
-                </button>
-            </div>
-        </div>
-    );
+				savingDocument.storageURL = String(downloadURL);
+			}
+
+			console.log("saving this document --> ", savingDocument);
+
+			await docRef.set({
+				savingDocument,
+			});
+
+			router.push(`/code?code=${docRef.id}`);
+
+			setIsUploading(true);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsText(false);
+			setTextContent("");
+			setIsFiles(false);
+			setIsUploading(false);
+		}
+	};
+
+	const handleChange = (value: React.ChangeEvent<HTMLInputElement>) => {
+		const allFields = value.target.files;
+
+		setFiles([...Object.values(allFields)]);
+	};
+
+	const removeFile = (index: number) => {
+		console.log(index, files);
+		const updatedFiles = [...files];
+
+		const newObj = updatedFiles.filter((_file, i) => index !== i);
+
+		setFiles([...newObj]);
+	};
+
+	useEffect(() => {
+		return () => {
+			firebase.auth().signOut();
+		};
+	}, []);
+
+	console.log(files);
+
+	return (
+		<div className="w-screen h-screen bg-blue-50 overflow-scroll">
+			<div className="text-4xl py-5 text-center font-bold font-sans">
+				Upload files
+			</div>
+			<div className="text-xl py-5 w-3/4 font-sans flex mx-auto justify-center mb-10 text-center">
+				Your data will be uploaded anonymously and will be deleted
+				automatically after 24 hours
+			</div>
+			<div className="flex mx-auto w-1/2 flex-col">
+				<div>
+					<label className="inline-flex items-center">
+						<input
+							disabled={isUploading}
+							type="checkbox"
+							className="form-checkbox text-blue-600"
+							checked={isText}
+							onChange={(e) => {
+								if (!e.target.checked) {
+									setTextContent("");
+								}
+								setIsText(e.target.checked);
+							}}
+						/>
+						<span className="ml-2">
+							Do you have text content that you want to share?
+						</span>
+					</label>
+				</div>
+				{isText && (
+					<div className="my-4">
+						<textarea
+							disabled={isUploading}
+							value={textContent}
+							onChange={(e) => setTextContent(e.target.value)}
+							className="form-textarea resize-y border rounded-md w-full max-w-full"
+						/>
+					</div>
+				)}
+				<div>
+					<label className="inline-flex items-center">
+						<input
+							disabled={isUploading}
+							type="checkbox"
+							className="form-checkbox text-blue-600"
+							checked={isFiles}
+							onChange={(e) => setIsFiles(e.target.checked)}
+						/>
+						<span className="ml-2">
+							Do you have files that you want to share?
+						</span>
+					</label>
+				</div>
+
+				{isFiles && (
+					<>
+						<div className="flex w-full my-8 items-center justify-center ">
+							<label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue-300 cursor-pointer hover:bg-blue-400 hover:text-white">
+								<svg
+									className="w-8"
+									fill="currentColor"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 20 20"
+								>
+									<path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
+								</svg>
+								<span className="mt-2 text-base leading-normal">
+									Select a file
+								</span>
+								<input
+									type="file"
+									multiple
+									onChange={handleChange}
+									hidden
+								/>
+							</label>
+						</div>
+
+						<div className="flex flex-col justify-center max-h-80 overflow-y-auto">
+							{files &&
+								Object.values(files)?.map((file, index) => (
+									<div
+										key={index}
+										className=" my-1 flex items-center w-1/2 bg-blue-100 p-1 m-auto"
+									>
+										<div className=" overflow-hidden px-1 whitespace-pre overflow-ellipsis rounded-sm bg-blue-200">
+											{file.name
+												.split(".")
+												.slice(0, -1)
+												.join(".")}
+										</div>
+										<div className="w-10 ml-2 rounded-sm px-1  flex items-center bg-blue-200">
+											{file.name.split(".").slice(-1)}
+										</div>
+										<div
+											className="w-min ml-2 rounded-sm p-1 flex items-center bg-blue-200 hover:bg-blue-300 cursor-pointer"
+											onClick={() => removeFile(index)}
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												className="h-4 w-4"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													strokeWidth="2"
+													d="M6 18L18 6M6 6l12 12"
+												/>
+											</svg>
+										</div>
+									</div>
+								))}
+						</div>
+					</>
+				)}
+
+				<div className="w-full bg-gray-200 h-0.5 mt-3 rounded-sm" />
+
+				<button
+					disabled={!(isText || isFiles)}
+					onClick={uploadDate}
+					className={`my-5 w-max mx-auto py-2 px-4 rounded-sm text-white focus:ring-1 flex ${
+						isUploading ? "bg-gray-400" : ""
+					} ${isText || isFiles ? "bg-blue-500" : "bg-gray-200"}`}
+				>
+					Ready to share
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Upload;
