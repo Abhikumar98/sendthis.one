cc89c6d869acafe936a2fdfaa1e3fa37b9d41f8d
AbhiKumar98 <abhishekkumar35962@gmail.com>
2021-01-30 15:28:54 +0530

    fix: index text content

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,38 @@ const Home = () => {
 
 	return (
 		<div className="w-screen h-screen ">
+			<div className="text m-4 sm:m-6 md:mx-20 md:my-8">
+				<div>Hey there ! 👋 </div>
+				<div>
+					Looking to share data without sharing your personal
+					information ?
+				</div>
+				<div className="font-semibold my-2">
+					{" "}
+					Well you came to the right place ✨
+				</div>
+				<div>
+					With <b>ShareWithMe</b> you can share your data let it be
+					any file or text with anyone without sharing any personal
+					contact information ! 💯 anonymous
+				</div>
+				<div>
+					You don&apos;t have to share it with us as well.
+					<ul className="my-4">
+						<li>↪ Upload</li>
+						<li>↪ Set a password ( optional )</li>
+						<li>↪ Get the qr code and unique code</li>
+						<li>↪ Share </li>
+					</ul>
+					and done !
+				</div>
+				<div>
+					Also, we won't keep your data forever either. Set a time
+					after which you want to delete your data and *swish* its
+					gone
+				</div>
+			</div>
+
 			<div className="flex mt-10 items-center justify-center ">
 				<Link href="/upload">
 					<Button>Share</Button>
